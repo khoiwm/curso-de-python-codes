fe2fbd3e7ec50fec891b4c086f3c4e5050fbc44a
khoi <126974024+khoiwm@users.noreply.github.com>
2023-03-04 13:28:48 -0300

    Update aula #008 exerc016.py

--- aula #008 exerc016.py
+++ aula #008 exerc016.py
@@ -1,5 +1,3 @@
-# crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira
-# ex: digite um número: 6.127, o número tem a parte inteira 6
 import math
 num = float(input('digite um número Real (ex: 6.5): '))
 inteiro = math.floor(num)
