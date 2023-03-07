21786a70c473708d10e1e4ecc6a094f0f8015b0d
khoi <126974024+khoiwm@users.noreply.github.com>
2023-03-07 10:27:11 -0300

    Add files via upload

---
+++ desafio 021.py
@@ -0,0 +1,9 @@
+# faça um programa em python que abra
+# e reproduza o áudio de um arquivo MP3
+
+import pygame
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('DLMLU.mp3')
+pygame.mixer.music.play(loops=0, start=0.0)
+pygame.event.wait()
